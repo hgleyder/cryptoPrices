687123bccdfb5f2cac08cc3d7a4585228ecfd45b
Gleyder Hernandez <gleyderh@gmail.com>
2017-11-30 17:54:28 -0400

    Filtro listo

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Container, Header, Content, Title, List, ListItem, Thumbnail, Text, Body } from 'native-base';
+import { StyleSheet, Dimensions,View } from 'react-native';
+import { Container, Header, Content, Title, List, ListItem, Thumbnail, Spinner, Item, Input, Text, Body } from 'native-base';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+        backgroundColor: "#19B5FE",
       coinsList: [],
+        coins: [],
+        text: "",
   }
 }
 
@@ -15,10 +18,10 @@ componentDidMount(){
 }
 
 getCoinsFromApiAsync =() => {
-    return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=40')
+    return fetch('https://api.coinmarketcap.com/v1/ticker')
       .then((response) => response.json())
       .then((responseJson) => {
-        this.setState({coinsList: responseJson});
+        this.setState({coins: responseJson});
       })
       .catch((error) => {
         console.error(error);
@@ -29,28 +32,82 @@ getCoinsFromApiAsync =() => {
 
 const contentCoins = () =>
 {
- return this.state.coinsList.map(coin =>{
-  return (
-  <ListItem key={coin.symbol} id={coin.symbol}>
-  <Thumbnail square size={80} />
-  <Body>
-  <Text>{coin.symbol}</Text>
-  <Text note>{coin.name}</Text>
-  <Text note>${coin["price_usd"]} USD</Text>
-  </Body>
-  </ListItem>
-  );
-});
-}
+ return this.state.coins.map(coin =>{
+    if(this.state.text!== "" && coin["name"].toLowerCase().indexOf(this.state.text.toLowerCase()) !== -1) {
+        return (
+                    <ListItem key={coin.symbol} id={coin.symbol}>
+                        <Body>
+                        <Text>{coin.symbol}</Text>
+                        <Text note style={{fontSize: 18}}>{coin.name}</Text>
+                        <View style={{flexDirection: "row", flex: 1}}>
+                            <Text note style={{color: "#1E8BC3"}}>${coin["price_usd"]} USD</Text>
+                            <Text note style={{color: "#1E8BC3"}}>{coin["price_btc"]} BTC</Text>
+                        </View>
+                        <Text note>Percent Change:</Text>
+                        <View style={{flexDirection: "row", flex: 1}}>
+                            <Text note
+                                  style={{color: parseFloat(coin["percent_change_1h"]) > 0 ? "#279b50" : "#c3233f"}}>{coin["percent_change_1h"]}%
+                                1h</Text>
+                            <Text note
+                                  style={{color: parseFloat(coin["percent_change_24h"]) > 0 ? "#279b50" : "#c3233f"}}>{coin["percent_change_24h"]}%
+                                24h</Text>
+                            <Text note
+                                  style={{color: parseFloat(coin["percent_change_7d"]) > 0 ? "#279b50" : "#c3233f"}}>{coin["percent_change_7d"]}%
+                                7d</Text>
+                        </View>
+                        </Body>
+                    </ListItem>
+                    )
+            }
+     else if(this.state.text === "") {
+         return (
+             <ListItem key={coin.symbol} id={coin.symbol}>
+                 <Body>
+                 <Text>{coin.symbol}</Text>
+                 <Text note style={{fontSize: 18}}>{coin.name}</Text>
+                 <View style={{flexDirection: "row", flex: 1}}>
+                     <Text note style={{color: "#1E8BC3"}}>${coin["price_usd"]} USD</Text>
+                     <Text note style={{color: "#1E8BC3"}}>{coin["price_btc"]} BTC</Text>
+                 </View>
+                 <Text note>Percent Change:</Text>
+                 <View style={{flexDirection: "row", flex: 1}}>
+                     <Text note
+                           style={{color: parseFloat(coin["percent_change_1h"]) > 0 ? "#279b50" : "#c3233f"}}>{coin["percent_change_1h"]}%
+                         1h</Text>
+                     <Text note
+                           style={{color: parseFloat(coin["percent_change_24h"]) > 0 ? "#279b50" : "#c3233f"}}>{coin["percent_change_24h"]}%
+                         24h</Text>
+                     <Text note
+                           style={{color: parseFloat(coin["percent_change_7d"]) > 0 ? "#279b50" : "#c3233f"}}>{coin["percent_change_7d"]}%
+                         7d</Text>
+                 </View>
+                 </Body>
+             </ListItem>
+         )
+     }
+    return null;
+})};
+
     return (
       <Container>
-        <Header>
+        <Header style={{backgroundColor:this.state.backgroundColor}}>
            <Text style={{marginTop: 15, color: 'white', fontSize: 22, fontWeight: '700'}}>Crypto Prices</Text>
           </Header>
         <Content>
-          <List>
-            {contentCoins()}
-          </List>
+            {this.state.coins.length?
+                <View>
+                <Item regular>
+                    <Input placeholder='Filter Coins' onChangeText={(text) => {this.setState({text:text})}}
+                           placeholderTextColor={"gray"}
+                           value={this.state.text}/>
+                </Item>
+                <List>
+                    {contentCoins()}
+                </List>
+                </View>
+                :
+                <Spinner color='#1E8BC3' />
+            }
         </Content>
       </Container>
       );
